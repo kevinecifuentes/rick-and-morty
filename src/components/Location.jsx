6bcb898bc7306be69ec04caba2301fa45ba3367f
Kevin Cifuentes <kevmusic25@gmail.com>
2023-06-04 22:21:01 -0500

    styles ended

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,67 +1,43 @@
 import axios from "axios"
-import { useEffect, useState } from "react"
 
 const Location = ({ location, setLocation }) => {
 
   //? Input no controlado 
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault()
-  //   const newLocation = e.target.newLocation.value
-
-  //   const URL = `https://rickandmortyapi.com/api/location/${newLocation}`
-
-  //   axios.get(URL)
-  //     .then(({ data }) => setLocation(data))
-  //     .catch((err) => console.log(err))
-  // }
-
-  //? Input controlado 
-  const [getLocation, setGetLocation] = useState('')
-  // const [searchDimenson, setSearchDimenson] = useState(second)
-
-  const handleLocation = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
-    const newLocationControlled = e.target.value
+    const newLocation = e.target.newLocation.value
 
-      `https://rickandmortyapi.com/api/location/?name${value}`
-
-    const URL = `https://rickandmortyapi.com/api/location/${newLocationControlled}`
+    const URL = `https://rickandmortyapi.com/api/location/${newLocation}`
 
     axios.get(URL)
-      .then(({ data }) => console.log(data))
+      .then(({ data }) => setLocation(data))
       .catch((err) => console.log(err))
   }
-
-  useEffect(() => {
-
-  }, [])
-
-
 
   return (
     <section className="bg-[url(/images/background-header.png)] bg-cover min-h-[200px] bg-center w-[100%]">
       <div className="bg-[url(/images/logo.png)] h-[250px] bg-cover bg-center flex min-[700px]:h-[350px] min-[1024px]:h-[450px]">
       </div>
       {/* Input de búsqueda no controlado */}
-      {/* <form className="flex justify-center justify-items-center py-5" onSubmit={handleSubmit}>
-        <input id='newLocation' className="px-1 text-black" placeholder="Type a location id" type="text" />
-        <button className="bg-red-500">Search<i className="bx bx-search ml-2"></i></button>
-      </form> */}
-
-      {/* Inpunt controlado  */}
-      <form className="flex justify-center justify-items-center py-6">
-
-        <input value={getLocation} onChange={handleLocation} className="outline-none border-2 border-green-500 px-1 py-3 w-70 text-center bg-black/80 text-white min-[1000px]: text-lg  " placeholder="Type a location id..." type="text" autoComplete="off " />
-
+      <form className="flex justify-center justify-items-center mb-9 mx-8 min-[700px]:mx-20" onSubmit={handleSubmit}>
+        <input id='newLocation'
+          className="w-[100%] outline-none border-2 border-green-500 px-1 py-3 text-center bg-black/80 text-white text-lg min-[700px]:text-xl min-[1000px]:text-2xl"
+          placeholder="Type a location id..."
+          type="text"
+          autoComplete="off" />
         <button className="bg-green-500 p-4"><img src='/search-icon.png' alt="" /></button>
-
       </form>
 
-      {/* Info de location  */}
-      <section className="bg-black/80 border-2 border-green-400 flex flex-col mx-8 py-8 items-center gap-2 capitalize ">
+      {/* Tile */}
+      <div className="flex justify-center">
+        <h2 className="text-green-500 text-xl min-[700px]:text-3xl mb-10">!Welcome to the crazy universe!</h2>
+      </div>
+
+      {/* Info de location */}
+      <section className="bg-black/80 border-2 border-green-400 flex flex-col mx-10 py-8 items-center gap-4 capitalize min-[700px]:mx-20">
         <h2 className="text-center text-2xl text-green-400">{location?.name}</h2>
-        <ul className="text-xl flex flex-wrap gap-4 p-4">
+        <ul className="text-xl flex flex-wrap gap-6 p-4">
           <li><b className="text-gray-500">type:</b> {location?.type}</li>
           <li><b className="text-gray-500">dimension:</b> {location?.dimension}</li>
           <li><b className="text-gray-500">population:</b> {location?.residents.length}</li>
